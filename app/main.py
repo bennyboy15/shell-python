7342c5a2f5d5397bc5e17dc1c098b575cb295858
bennyboy15 <benjaminharvey2000@gmail.com>
2026-03-06 11:39:14 +1030

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 
 def main():
+    builtIns = ["exit", "echo", "type"]
     while (True):
         command = input("$ ").strip()
         func = command.split(" ")[0].strip()
@@ -11,6 +12,13 @@ def main():
                 continue
             case "echo":
                 print(command[5:])
+                continue
+            case "type":
+                args = command[4:].strip()
+                if args in builtIns:
+                    print(f"{args} is a shell builtin")
+                else:
+                    print(f"{args}: not found")
                 continue
             case _:
                 print(f"{command}: command not found")
