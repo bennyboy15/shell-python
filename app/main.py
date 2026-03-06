7e67a9ae2aff463a1d1ca1f129225827f6c3e858
bennyboy15 <benjaminharvey2000@gmail.com>
2026-03-06 11:27:13 +1030

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,7 +3,13 @@ import sys
 def main():
     while (True):
         command = input("$ ")
-        print(f"{command}: command not found")
+        match(command):
+            case "exit":
+                sys.exit()
+                continue
+            case _:
+                print(f"{command}: command not found")
+                continue
 
 
 if __name__ == "__main__":
